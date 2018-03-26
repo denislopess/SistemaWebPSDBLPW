bea4baec66c1cc7a3c2ebd86969e7f20a6555e39
Denis Lopes Da Silva <33581819+denislopess@users.noreply.github.com>
2018-03-26 19:33:38 -0300

    Update connection.php

--- Config/connection.php
+++ Config/connection.php
@@ -1,6 +1,6 @@
 <?php
 
 
-$con = new PDO("pgsql:host=127.0.0.1;dbname=empresa",'postgres','denis1994');
+$con = new PDO("pgsql:host=127.0.0.1;dbname=empresa",'postgres','');
 $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $con->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
